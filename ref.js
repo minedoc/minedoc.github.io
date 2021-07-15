6f202cfdb8ab3ae141a7a092445eb205c83e428a
david <david>
2021-07-15 22:01:56 +1000

    simplify ref

--- ref.js
+++ ref.js
@@ -1,185 +1,160 @@
-// no early stopping
-//   equality check is expensive, maybe some super heavy nodes want this
-//   converts evaluation from leaf up to root down (quite difficult)
-//   measure if this is common in practice
-// no garbage collection yet
-//   all computations will remain forever
-//
-// can we hook directly to UI?
+// design choices
+//   pull driven
+//   no early stopping - requires push based + equality compare
+//   no manual recompute() - getting consistency is hard (cant use integer wave, would need to eager evaluation?)
 
 const computeStack = [];
+const DELETE = Symbol('DELETE');
+const FULL_UPDATE = Symbol('FULL_UPDATE');
 
 function baseRef(constructor, methods) {
   return function(init) {
-    const outputs = new Set();
-    let inner = constructor(init, outputs);  // old inners can still trigger change events!
-    return Object.assign({
-      get value() {
-        saveOutputs(outputs);
-        return inner;
-      },
-      set value(init) {
-        if (init != inner) {
-          outputs.forEach(output => output(undefined));
-          inner = constructor(init, outputs);
-        }
-      },
-    }, methods);
+    const outputs = new NotifySet();
+    let value = constructor(init);
+    const get = () => {
+      outputs.saveCaller();
+      return value;
+    };
+    const set = val => {
+      value = constructor(val);
+      outputs.notify(FULL_UPDATE);
+    };
+    return Object.assign(get, {set}, methods(get, outputs));
   };
 }
 
-const ref = baseRef((init, outputs) => freeze(init));
+const ref = baseRef(init => freeze(init), (get, outputs) => null);
+const refMap = baseRef(init => new ReadOnlyMap(init), (ref, outputs) => ({
+  ...mapMethods(ref),
+  set(key, value) { outputs.notify(key); return Map.prototype.set.call(ref(), key, freeze(value)); },
+  delete(key) { outputs.notify(key); return Map.prototype.delete.call(ref(), key); },
+  clear() { outputs.notify(FULL_UPDATE); return Map.prototype.clear.call(ref()); },
+}));
 
-const mapMethods = {
-  select(fn) { return select(this, fn) },
-  filter(fn) { return filter(this, fn) },
-  groupBy(fn) { return groupBy(this, fn) },
-};
-
-const refMap = baseRef((init, outputs) => new RefMap(init, outputs), mapMethods);
-
-function computed(updateAll, updateKeys) {
-  let inner = new ReadOnlyMap();
-  let sync = false;
-  const syncKeys = new Set();
-  const outputs = new Set();
-  const notify = key => {
-    if (sync) {
-      if (updateKeys === undefined || key === undefined) {
-        sync = false;
-        outputs.forEach(output => output(undefined));
-      } else {
-        syncKeys.add(key);
-        outputs.forEach(output => output(key));
-      }
+function computed(update, updateKeys, period=0) {
+  let value = undefined;
+  let dirty = true;
+  let keys = [];
+  const outputs = new NotifySet();
+  const notify = debounce(period, key => {  // TODO: would like to check !dirty but doesn't work
+    if (updateKeys === undefined || key === FULL_UPDATE) {
+      dirty = true;
+      outputs.notify(FULL_UPDATE);
+    } else {
+      keys.push(key);
+      outputs.notify(key);
     }
-  };
-  return {
-    get value() {
-      saveOutputs(outputs);
-      if (!sync || syncKeys.size > 0) {
-        computeStack.push(notify);
-        if (sync) {
-          updateKeys(inner, syncKeys); console.log('updateKeys', syncKeys, inner);
+  });
+  return () => {
+    outputs.saveCaller();
+    if (dirty || keys.length > 0) {
+      computeStack.push(notify);
+      try {
+        if (dirty) {
+          value = update(); // console.log('update', value);
         } else {
-          inner = updateAll(inner); console.log('updateAll', inner);
+          updateKeys(value, keys); // console.log('updateKeys', keys, value);
         }
+      } finally {
         computeStack.pop();
-        sync = true;
-        syncKeys.clear();
       }
-      return inner;
-    },
-    set value(init) { throw 'cannot set computed' },
+      dirty = false;
+      keys.length = 0;
+    }
+    return value;
   };
 }
 
-export {ref, refMap, computed};
-
-function select(srcRef, fn) { // k,v -> (k,v -> Maybe u) -> k,u
-  function updateKeys(dest, keys) {
-    const src = srcRef.value;
-    for (const key of keys) {
-      const value = src.get(key);
-      mapSave(dest, key, value === undefined ? undefined : fn(key, value));
-    }
-  }
-  function updateAll(dest) {
-    dest[privateMethod.clear]();
-    updateKeys(dest, srcRef.value.keys());
-    return dest;
-  }
-  return Object.assign(computed(updateAll, updateKeys), mapMethods);
+function computedMap(fn, period=0) {
+  const output = computed(fn, undefined, period);
+  return Object.assign(output, mapMethods(output));
 }
 
-function filter(srcRef, fn) {
-  return select(srcRef, (k, v) => fn(k, v) ? v : undefined);
-}
+export {ref, refMap, computed, computedMap, DELETE};
 
-function join(srcRefs, fn) {  // k,v -> k,u -> (v,u -> Maybe w) -> k,w
-  function updateKeys(dest, keys) {
-    const srcs = srcRefs.map(r => r.value);
-    for (const key of keys) {
-      const value = srcs[0].get(key);
-      mapSave(dest, key, value === undefined ? undefined : fn(key, ...vals.map(v => v.get(key)));
+class NotifySet {
+  constructor() {
+    this.clients = new IterableWeakSet();
+  }
+  saveCaller(x) {
+    if(computeStack.length > 0) {
+      this.clients.add(computeStack[computeStack.length - 1]);
     }
   }
-  function updateAll(dest) {
-    dest[privateMethod.clear]();
-    updateKeys(dest, refs[0].value.keys());
+  notify(value) {
+    this.clients.forEach(fn => fn(value));
+  }
+}
+
+const mapMethods = ref => ({
+  map(fn) { return join(ref, [], fn) },
+  filter(fn) { return join(ref, [], (k, v) => fn(k, v) ? v : DELETE) },
+  join(refs, fn) { return join(ref, refs, fn) },
+  groupBy(fn) { return groupBy(ref, fn) },
+})
+
+function join(srcRef, joinRefs, fn) {  // {k,v} -> [{k,u}] -> (v,[u] -> w) -> k,w
+  function updateJoin() {
+    const src = srcRef();
+    return insert(src, new ReadOnlyMap(), src.keys());
+  }
+  function updateJoinByKey(dest, keys) {
+    insert(srcRef(), dest, keys);
+  }
+  function insert(src, dest, keys) {
+    const join = joinRefs.map(x => x());
+    for (const key of keys) {
+      mapPut(dest, key, src.has(key) ? fn(key, src.get(key), ...join.map(j => j.get(key))) : DELETE);
+    }
     return dest;
   }
-  return Object.assign(computed(updateAll, updateKeys), mapMethods);
+  const output = computed(updateJoin, updateJoinByKey);
+  return Object.assign(output, mapMethods(output));
 }
 
 function groupBy(srcRef, fn) {  // k,v -> (k,v -> Map<j,u>) -> j,Map<k,u>
   const keyMap = new Map();
-  const getDef = (dest, key) => dest.get(key) ?? dest[privateMethod.set](key, new Map());
-  const setMap = (dest, key, value) => { dest.set(key, value); return value };
-  function insert(src, dest, key) {
-    const value = src.get(key);
-    if (value !== undefined) {
-      for (const [newKey, newValue] of setMap(keyMap, key, fn(key, value))) {
-        getDef(dest, newKey).set(key, newValue);
-      }
-    }
-  }
-  function updateKeys(dest, keys) {
-    const src = srcRef.value;
-    for (const key of keys) {
-      const pairs = keyMap.get(key);
-      if (pairs !== undefined) {
-        for (const [newKey, newValue] of pairs) {
-          const dmap = dest.get(newKey);
-          if (dmap.size > 1) {
-            dmap.delete(key);
-          } else {
-            dest[privateMethod.delete](newKey);
-          }
-        }
-        pairs.clear();
-      }
-      insert(src, dest, key);
-    }
-  }
-  function updateAll(dest) {
+  const keyMapDelete = key => { const a = keyMap.get(key); keyMap.delete(key); return a || []; };
+  const keyMapSet = (key, value) => { keyMap.set(key, value); return value };
+  function updateGroupby() {
+    const src = srcRef();
     keyMap.clear();
-    dest[privateMethod.clear]();
-    const src = srcRef.value;
-    for (const key of src.keys()) {
-      insert(src, dest, key);
+    return insert(src, new ReadOnlyMap(), src.keys());
+  }
+  function updateGroupbyByKey(dest, keys) {
+    insert(srcRef(), dest, keys);
+  }
+  function insert(src, dest, keys) {
+    const get = key => dest.get(key) ?? mapPut(dest, key, new Map());
+    for (const key of keys) {
+      for (const newKey of keyMapDelete(key)) {
+        get(newKey).delete(key);
+      }
+      for (const [newKey, newValue] of src.has(key) ? keyMapSet(key, fn(key, src.get(key))) : []) {
+        get(newKey).set(key, newValue);
+      }
     }
     return dest;
   }
-  return Object.assign(computed(updateAll, updateKeys), mapMethods);
+  const output = computed(updateGroupby, updateGroupbyByKey);
+  return Object.assign(output, mapMethods(output));
 }
 
-function saveOutputs(outputs) {
-  if(computeStack.length > 0) {
-    outputs.add(computeStack[computeStack.length - 1]);
-  }
-}
+// utility
 
-const privateMethod = { set: Symbol(), delete: Symbol(), clear: Symbol(), notify: Symbol() };
-
-class InternalMap extends Map {
-  [privateMethod.set](key, value) { super.set(key, value); return value; }
-  [privateMethod.delete](key) { return super.delete(key); }
-  [privateMethod.clear]() { super.clear(); }
-}
-
-class RefMap extends InternalMap {
-  constructor(init, outputs) { super(init); this.outputs = outputs; }
-  [privateMethod.notify](key) { if (this.outputs) { this.outputs.forEach(output => output(key)) } }
-  set(key, value) { this[privateMethod.notify](key); return super.set(key, freeze(value)); }
-  delete(key) { this[privateMethod.notify](key); return super.delete(key); }
-  clear() { this[privateMethod.notify](undefined); super.clear(); }
-}
-
-class ReadOnlyMap extends InternalMap {
+class ReadOnlyMap extends Map {
   set(key, value) { throw 'cannot modify ReadOnlyMap' }
   delete(key) { throw 'cannot modify ReadOnlyMap' }
   clear(key) { throw 'cannot modify ReadOnlyMap' }
+}
+
+function mapPut(map, key, value) {
+  if (value === DELETE) {
+    Map.prototype.delete.call(map, key);
+  } else {
+    Map.prototype.set.call(map, key, value);
+  }
+  return value;
 }
 
 function freeze(x) {
@@ -190,11 +165,49 @@ function freeze(x) {
   return x;
 }
 
-function mapSave(dest, key, value) {
-  if (value == undefined) {
-    dest[privateMethod.delete](key);
-  } else {
-    dest[privateMethod.set](key, value);
+class IterableWeakSet {
+  constructor() {
+    this.setWeak = new Set();  // to be iterated
+    this.weakSet = new WeakSet();  // prevent add() duplicates
+  }
+  add(item) {
+    if (!this.weakSet.has(item)) {
+      this.setWeak.add(new WeakRef(item));
+      this.weakSet.add(item);
+    }
+  }
+  forEach(fn) {
+    this.setWeak.forEach(x => {
+      const value = x.deref();
+      if (value) {
+        fn(value);
+      } else {
+        this.setWeak.delete(x);
+      }
+    });
   }
 }
 
+function debounce(period, fn) {
+  if (period == 0) {
+    return fn;
+  } else {
+    let lastTime = 0;
+    const saved = [];
+    return param => {
+      if (lastTime < Date.now() - period) {
+        fn(param);
+        setTimeout(() => lastTime = Date.now(), 50);
+        setTimeout(() => {
+          if (saved.length > 0) {
+            saved.map(fn);
+            saved.length = 0;
+            setTimeout(() => lastTime = Date.now(), 50);
+          }
+        }, period);
+      } else {
+        saved.push(param);
+      }
+    };
+  }
+}
