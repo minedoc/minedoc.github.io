69e9cb0eeb4390ee542ec5cc9e0197952527c11d
minedoc <79396769+minedoc@users.noreply.github.com>
2022-08-08 16:38:23 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,37 @@
 self.addEventListener('install', () => self.skipWaiting());
+
+const cacheName = 'offline_files';
+const precachedAssets = [
+  'index.html',
+  'index.css',
+  'diff.js',
+  'main.js',
+  'database.js',
+  'lib.js',
+  'ref.js',
+  'changes.js',
+  'share.js',
+  'types.js',
+  'util.js',
+  'bloomfilter.js',
+  'discovery.js',
+  'stub.js',
+  'binary.js',
+  'manifest.json',
+  'site-icon.svg',
+];
+
+self.addEventListener('install', () => {
+  event.waitUntil(caches.open(cacheName).then(cache => cache.addAll(precachedAssets));
+});
+
+function canonicalPath(path) {
+  return path == '/' ? 'index.html' : path;
+}
+
+self.addEventListener('fetch', event => {
+  const path = canonicalPath(new URL(event.request.url).pathname);
+  if (precachedAssets.includes(path)) {
+    event.respondWith(caches.open(cacheName).then(cache => cache.match(path));
+  }
+});
